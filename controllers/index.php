ed59b32ddc46c2d1b8bf8a3e0a4b6dd2b69f9542
onizu <onizu.db@gmail.com>
2019-05-12 07:01:36 +0530

    required bootstrap

--- controllers/index.php
+++ controllers/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__.'/../functions.php';
+require_once __DIR__.'/../core/bootstrap.php';
 // scriptLocation('controllers/index.php');
 
 $tasks = $app['database']->selectAll('tasks');
